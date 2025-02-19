8d87545a2a98e7bddfa01351b2341c9595da9527
laudixelohombre <91198535+laudixelohombre@users.noreply.github.com>
2025-02-19 03:55:13 +0100

    Update BiometricAuth.kt

--- NFC-Payments-App/app/src/main/java/com/example/nfcapp/BiometricAuth.kt
+++ NFC-Payments-App/app/src/main/java/com/example/nfcapp/BiometricAuth.kt
@@ -1,7 +1,6 @@
 package com.example.nfcapp
 
 import android.content.Context
-import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
 
